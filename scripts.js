e8601996b68b28b8085055ae7ac863bea95291c4
RamraoD <ramraodesai009@gmail.com>
2020-09-09 17:02:14 +0530

    updated responsive

--- scripts.js
+++ scripts.js
@@ -1,24 +1,25 @@
 $(document).ready(function () {
     $.getJSON("https://gorest.co.in/public-api/users", function (data) {
         console.log(data);
-
         for (var key in data) {
             for (var value in data[key]) {
                 var dataList =
-                    '<div class="row"><div class="col-1 data item">' + data[key][value].id + '</div> <div class=col-3 name item">' + data[key][value].name + '</div>  <div class=col-3 email item">' + data[key][value].email + '</div> <div class=col-3 gender item">' + data[key][value].gender + '</div> <div class=col-2 status item">' + data[key][value].status + '</div></div> '
+                    '<div class="row"><div class="col-1 d-none d-md-block data item">' + data[key][value].id + '</div> <div class="col-3 name item">' + data[key][value].name + '</div>  <div class="col-3 d-none d-lg-block email item">' + data[key][value].email + '</div> <div class="col-3 gender item">' + data[key][value].gender + '</div> <div class="col-2 status item">' + data[key][value].status + '</div></div> '
                 $("#myUL").append(dataList);
-            }
 
-            $('select').on('change', function () {
-                
-            });
+                // $.each(data[key][value], function (key, val) {
+                //     $('#gender').append('<option value="' + key + '">' + val.gender + '</option>');
+                // });
+
+                // $.each(data[key][value], function (key, val) {
+                //     $('#gender').append('<option value="' + key + '">' + val + '</option>');
+                // });
+                // var dropdown =
+                //     '<select id="gender"><option value="">Gender</option><option value=""> '+ data[key][value].gender +'</option></select>'
+                // $("#drop").append(dropdown);
+                // console.log(dropdown)
+            }
         }
-        $("#status").change(function () {
-            var status = this.value;
-            if (status == "Active")
-                console.log(status);
-            $("#icon_class, #background_class").hide();
-        });
     });
 
     // For search filter
@@ -38,3 +39,9 @@ $(document).ready(function () {
     // For even and odd color
     $("#myUL > div:nth-child(even)").addClass("even");
 });
+
+
+   // var dropdown =
+                //     '<select id="gender"><option value="">Gender</option><option value=""> '+ data[key][value].gender +'</option></select>'
+                // $("#drop").append(dropdown);
+                // console.log(dropdown)
